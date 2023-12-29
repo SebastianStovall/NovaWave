07d95cc6b92d364009307430da4af2e796f6d22a
SebastianStovall <sebastianstovall@gmail.com>
2023-12-29 00:49:41 -0600

    trying to clean up code for serving build from express

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -32,6 +32,23 @@ app.listen(8000, () => {
 
 app.use('/', router()) // router from router/index.ts
 
+
+// Static routes
+// Serve React build files in production
+if (process.env.NODE_ENV === 'production') {
+    // Serve the frontend's index.html file at the root route
+    app.get('/', (req, res) => {
+        return res.sendFile(
+            path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+        );
+    });
+
+    // Serve the static assets in the frontend's build folder
+    app.use('/', express.static(path.join(__dirname, '../../frontend/build')));
+}
+
+
+
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => { // cenertalized error handling if no routes are hit or middleware errors
   // handle errors
   if(err instanceof CustomError) {
@@ -40,3 +57,58 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => { // ce
     return res.status(500).json({ message: err.message });
   }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+// require('dotenv').config() // load env variables on entry
+
+// import express, { Request, Response, NextFunction } from "express";
+// import { json } from "body-parser"; // makes data accessible through req.body due to parsing of incoming request data
+// import cookieParser from "cookie-parser"; // parse cookie data (for session token)
+// import compression from 'compression'; // compresses size of response bodies before sending data (increases loading time and bandwidth usage)
+// import cors from 'cors'; // enable CORS for all routes, any client allowed to make requests to our server
+// import router from "./router";
+// import CustomError from "./utils/CustomError";
+// import { connectToMongoDB } from "./db/connect";
+// const path = require('path');
+
+// const app = express();
+
+// app.use(cors({ // enforce certain routes to require authentication (require session token)
+//   credentials: true,
+// }))
+
+// app.use(json());
+// app.use(compression());
+// app.use(cookieParser());
+
+
+// const port = 8000;
+// app.listen(8000, () => {
+//   (async () => {
+//     await connectToMongoDB()
+//   })();
+//   console.log(`Example app listening on port ${port}`)
+// });
+
+
+// app.use('/', router()) // router from router/index.ts
+
+// app.use((err: Error, req: Request, res: Response, next: NextFunction) => { // cenertalized error handling if no routes are hit or middleware errors
+//   // handle errors
+//   if(err instanceof CustomError) {
+//     return res.status(err.code).json({ message: err.name, error: err.message });
+//   } else {
+//     return res.status(500).json({ message: err.message });
+//   }
+// });
